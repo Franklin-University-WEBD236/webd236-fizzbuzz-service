2dea6bc26a7ddc7af821067c08b126955a7e558c
Glitch (webd236-blog-lab1-starter) <none>
2020-05-06 19:12:57 +0000

    📚😹 Checkpoint ./controllers/fizzbuzz.php:86128/1144

--- controllers/fizzbuzz.php
+++ controllers/fizzbuzz.php
@@ -3,20 +3,20 @@ require_once "include/util.php";
 // require_once "models/fizzbuzz.php";
 
 function post_check() {
-  $result = null;
-  $number = safeParam($_POST, 'number', false);
+  $result = 'NaN';
+  $number = (int)safeParam($_POST, 'number', false);
   if ($number) {
-    $number = (int)$number;
-    if (number % 3 == 0)
+    if ($number % 3 != 0 && $number % 5 != 0) {
+      $result = $number;
+    } else if ($number % 3 == 0 && $number % 5 == 0) {
+      $result = "FizzBuzz";
+    } else if ($number % 3 == 0) {
+      $result = "Fizz";
+    } else if ($number % 5 == 0) {
+      $result = "Buzz";
+    }
   }
-  // Put your code for post_check here, something like
-  // 1. Load and validate parameters or form contents
-  // 2. Query or update the database
-  // 3. Render a template or redirect
-  renderTemplate(
-    "views/fizzbuzzcheck.php",
-    array(
-      'title' => 'fizzbuzzcheck',
-    )
-  );
+  $result = array("result" => $result);
+  echo(json_encode($result));
+  exit();  
 }
