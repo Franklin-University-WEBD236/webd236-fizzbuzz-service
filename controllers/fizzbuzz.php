0ace67d228c4c7e9d9030332e6a8015cebcb3e9f
Glitch (webd236-blog-lab1-starter) <none>
2020-07-21 12:21:16 +0000

    👰🙍 Checkpoint

--- controllers/fizzbuzz.php
+++ controllers/fizzbuzz.php
@@ -43,3 +43,16 @@ function get_test() {
     )
   );
 }
+
+function get_test() {
+  // Put your code for get_test here, something like
+  // 1. Load and validate parameters or form contents
+  // 2. Query or update the database
+  // 3. Render a template or redirect
+  renderTemplate(
+    "views/fizzbuzztest.php",
+    array(
+      'title' => 'fizzbuzztest',
+    )
+  );
+}
